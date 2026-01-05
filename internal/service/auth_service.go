55d4f023fe08d836853272398cdbd978c86f60ee
Septian Adi Pratama <septianadip7998@gmail.com>
2026-01-05 07:42:59 +0700

    feat: add auth service for login and token generation

--- internal/service/auth_service.go
+++ internal/service/auth_service.go
@@ -4,34 +4,42 @@ import (
 	"context"
 	"errors"
 	"project-app-inventory-restapi-golang-iay/internal/entity"
+	"project-app-inventory-restapi-golang-iay/internal/repository"
 	"time"
 
 	"github.com/google/uuid"
 	"golang.org/x/crypto/bcrypt"
 )
 
+type AuthService struct {
+	repo *repository.UserRepository
+}
+
+func NewAuthService(repo *repository.UserRepository) *AuthService {
+	return &AuthService{repo: repo}
+}
+
 func (s *AuthService) Login(ctx context.Context, username, password string) (string, error) {
-	// 1. ambil username
-	user, err := s.userRepo.GetByUsername(ctx, username)
+	user, err := s.repo.GetByUsername(ctx, username)
 	if err != nil {
-		return "", errors.New("invalid credentials")
+		return "", errors.New("user not found")
 	}
 
-	// 2. Cek Password
 	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
 	if err != nil {
-		return "", errors.New("invalid credentials")
+		return "", errors.New("invalid password")
 	}
 
-	// 3. buat session uuid
 	token := uuid.NewString()
 	session := entity.Session{
 		Token:     token,
 		UserID:    user.ID,
-		ExpiredAt: time.Now().Add(24 * time.Hour), // Expire 1 hari
+		ExpiredAt: time.Now().Add(24 * time.Hour),
 	}
 
-	// 4. Simpan ke DB
-	err = s.sessionRepo.CreateSession(ctx, session)
-	return token, err
+	if err := s.repo.CreateSession(ctx, session); err != nil {
+		return "", err
+	}
+
+	return token, nil
 }
